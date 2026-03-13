47c6447de43615fac0ab386e1623503e647ab274
mytoandeptrai <67469007+mytoandeptrai@users.noreply.github.com>
2026-03-13 11:55:37 +0700

    fix: hotfix can not run dev

--- apps/api/src/matching/matching.module.ts
+++ apps/api/src/matching/matching.module.ts
@@ -5,6 +5,7 @@ import { OrderService } from './order.service';
 import { OrderEntity } from './entities/order.entity';
 import { OrderHistoryEntity } from './entities/order-history.entity';
 import { TradeEntity } from './entities/trade.entity';
+import { AccountEntity } from '../account/entities/account.entity';
 import { LoggerModule } from '../common/logger/logger.module';
 import { AccountModule } from '../account/account.module';
 import { OrderbookService } from './orderbook.service';
@@ -13,7 +14,12 @@ import { OrderbookService } from './orderbook.service';
   imports: [
     LoggerModule,
     AccountModule,
-    TypeOrmModule.forFeature([OrderEntity, OrderHistoryEntity, TradeEntity]),
+    TypeOrmModule.forFeature([
+      OrderEntity,
+      OrderHistoryEntity,
+      TradeEntity,
+      AccountEntity,
+    ]),
   ],
   controllers: [OrderController],
   providers: [OrderService, OrderbookService],
